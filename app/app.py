6e4c809654b9304cc453263dcbf1f9557d16b435
Ertshus <siverer@stud.ntnu.no>
2023-12-04 14:49:14 +0100

    Added new files and removed unused.

--- app/app.py
+++ app/app.py
@@ -1,32 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLite database file
+
+# Replace 'your_database_connection_string' with the actual connection string
+app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_connection_string'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), nullable=False)
-    last_name = db.Column(db.String(80), nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
 
-@app.route('/greet', methods=['POST'])
-def greet():
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
+        new_user = User(first_name=first_name, last_name=last_name)
+        db.session.add(new_user)
+        db.session.commit()
 
-    # Save the user to the database
-    new_user = User(first_name=first_name, last_name=last_name)
-    db.session.add(new_user)
-    db.session.commit()
+        return redirect(url_for('index'))
 
-    return render_template('greet.html', first_name=first_name, last_name=last_name)
+    users = User.query.all()
+    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
-    # Create the database tables before running the app
     db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "azure_subscription_id" {
+  description = "225af78d-89a6-45b3-ba8f-c242ee3116e1"
+}
+
+variable "azure_client_id" {
+  description = "Azure client ID"
+}
+
+# Define other required variables
