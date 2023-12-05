d94ff5293f18355c65df1a5b5a204ce467d8d3fd
Ertshus <siverer@stud.ntnu.no>
2023-12-05 17:38:59 +0100

    Test application.

--- app/app.py
+++ app/app.py
@@ -1,34 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask import make_response
 
 app = Flask(__name__)
 
-# Replace 'your_database_connection_string' with the actual connection string
-app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_connection_string'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+@app.route('/')
+def hello_world():
+    return 'Hello, World!!'
 
-db = SQLAlchemy(app)
+@app.route('/<page>')
+def default(page):
+    response = make_response('The page %s does not exist.' % page, 404)
+    return response
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-
-        new_user = User(first_name=first_name, last_name=last_name)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for('index'))
-
-    users = User.query.all()
-    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    
