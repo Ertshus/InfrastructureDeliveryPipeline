9895a3c30793db971c8e38af57a6cf15e59881ba
Ertshus <siverer@stud.ntnu.no>
2023-12-05 18:19:49 +0100

    Changes to the terraform file.

--- main.tf
+++ main.tf
@@ -8,23 +8,23 @@ resource "azurerm_resource_group" "DeliveryPipeline_rg" {
 }
 
 resource "azurerm_virtual_network" "DeliveryPipeline_vnet" {
-  name                = "portfolioVNet"
+  name                = "DeliveryPipelineVNet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.portfolio_rg.location
-  resource_group_name = azurerm_resource_group.portfolio_rg.name
+  location            = azurerm_resource_group.DeliveryPipeline_rg.location
+  resource_group_name = azurerm_resource_group.DeliveryPipeline_rg.name
 }
 
 resource "azurerm_subnet" "DeliveryPipeline_subnet" {
-  name                 = "portfolioSubnet"
-  resource_group_name  = azurerm_resource_group.portfolio_rg.name
-  virtual_network_name = azurerm_virtual_network.portfolio_vnet.name
+  name                 = "DeliveryPipelineSubnet"
+  resource_group_name  = azurerm_resource_group.DeliveryPipeline_rg.name
+  virtual_network_name = azurerm_virtual_network.DeliveryPipline_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "DeliveryPipeline_nsg" {
-  name                = "portfolioNSG"
-  location            = azurerm_resource_group.portfolio_rg.location
-  resource_group_name = azurerm_resource_group.portfolio_rg.name
+  name                = "DeliveryPipelineNSG"
+  location            = azurerm_resource_group.DeliveryPipeline_rg.location
+  resource_group_name = azurerm_resource_group.DeliveryPipeline_rg.name
 }
 
 resource "azurerm_network_security_rule" "DeliveryPipeline_nsg_rule" {
@@ -37,33 +37,33 @@ resource "azurerm_network_security_rule" "DeliveryPipeline_nsg_rule" {
   destination_port_range      = "80"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.portfolio_rg.name
-  network_security_group_name = azurerm_network_security_group.portfolio_nsg.name
+  resource_group_name         = azurerm_resource_group.DeliveryPipeline_rg.name
+  network_security_group_name = azurerm_network_security_group.DeliveryPipeline_nsg.name
 
 }
 
 resource "azurerm_subnet_network_security_group_association" "DeliveryPipeline_subnet_nsg_association" {
-  subnet_id                 = azurerm_subnet.portfolio_subnet.id
-  network_security_group_id = azurerm_network_security_group.portfolio_nsg.id
+  subnet_id                 = azurerm_subnet.DeliveryPipeline_subnet.id
+  network_security_group_id = azurerm_network_security_group.DeliveryPipeline_nsg.id
 }
 
 resource "azurerm_service_plan" "DeliveryPipeline_asp" {
-  name                = "portfolioAppServicePlan"
-  location            = azurerm_resource_group.portfolio_rg.location
-  resource_group_name = azurerm_resource_group.portfolio_rg.name
+  name                = "DeliveryPipelineAppServicePlan"
+  location            = azurerm_resource_group.DeliveryPipeline_rg.location
+  resource_group_name = azurerm_resource_group.DeliveryPipeline_rg.name
 
-  os_type   = "Linux"   # Change to "Windows" if required
-  sku_name  = "B1"      # Choose the appropriate SKU
+  os_type   = "Linux"   
+  sku_name  = "B1"      
 }
 
 resource "azurerm_app_service" "DeliveryPipeline_app"{
-  name                = "portfolioAppService"
-  location            = azurerm_resource_group.portfolio_rg.location
-  resource_group_name = azurerm_resource_group.portfolio_rg.name
-  app_service_plan_id = azurerm_service_plan.portfolio_asp.id
+  name                = "DeliveryPipelineAppService"
+  location            = azurerm_resource_group.DeliveryPipeline_rg.location
+  resource_group_name = azurerm_resource_group.DeliveryPipeline_rg.name
+  app_service_plan_id = azurerm_service_plan.DeliveryPipeline_asp.id
 
   site_config {
-    dotnet_framework_version = "v4.0" # Update as needed or remove for non-.NET apps
+    dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
 
