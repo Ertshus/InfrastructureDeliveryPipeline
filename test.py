c1fa74432d8d378ed967171a491b7d52f2a9bc81
Ertshus <siverer@stud.ntnu.no>
2023-12-06 23:22:02 +0100

    Test

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 import unittest
 from app import app
 
+
+"""
+Tests that the HTTP packet recieved is OK status code 200.
+
+"""
+
+
 class AppTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
     def test_index(self):
-        response = self.app.get('/')
+        tester = app.test_client(self)
+        response = tester.get("/", content_type="html/text")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!!!!!')
+        self.assertEqual(
+            response.data, "Hello, World!!!!!!"
+        )
 
-if __name__ == '__main__':
-  unittest.main()
+    def test_default(self):
+        tester = app.test_client(self)
+        response = tester.get("xyz", content_type="html/text")
+        self.assertEqual(response.status_code, 404)
+        self.assertTrue(b"does not exist" in response.data)
+
+
+if name == "main":
+    unittest.main()
