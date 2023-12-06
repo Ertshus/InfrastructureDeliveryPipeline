9dc97941409d22e20dfc9be9853c70e86393a881
Ertshus <siverer@stud.ntnu.no>
2023-12-06 23:18:06 +0100

    Tester

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class AppTestCase(unittest.TestCase):
     def test_index(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!!!!')
+        self.assertEqual(response.data, b'Hello, World!!!!!')
 
 if __name__ == '__main__':
   unittest.main()
