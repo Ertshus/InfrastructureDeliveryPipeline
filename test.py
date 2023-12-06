58e29618be4287f3e95a1913c657632ceed82c41
Ertshus <siverer@stud.ntnu.no>
2023-12-06 23:05:08 +0100

    Test

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ from app import app
 
 class AppTestCase(unittest.TestCase):
   def test_index(self):
-    tester = app.test_client(self)
+    tester = app.client(self)
     response = tester.get('/', content_type='html/text')
     self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data, b'Hello World!!!!!!!!')
+    self.assertEqual(response.data, b'Hello World!!!!!!!!!')
 
 if __name__ == '__main__':
   unittest.main()
